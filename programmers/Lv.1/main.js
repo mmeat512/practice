927450ea27f2083a3d17725afc6c9b726387d37d
mmeat512 <wndus0958@naver.com>
2023-03-03 22:50:44 +0900

    codingtest init

--- programmers/Lv.1/main.js
+++ programmers/Lv.1/main.js
@@ -1,1 +1,12 @@
-console.log('level1');
+(() => {
+  function log(val) {
+    console.log(val);
+  }
+
+  function solution(number) {
+    var answer = 0;
+    return answer;
+  }
+
+  log(solution([-2, 3, 0, 2, -5]));
+})();
