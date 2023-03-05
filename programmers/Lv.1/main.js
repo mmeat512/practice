78f6557257ace4b4bb9b671fc97016c3c8154a72
mmeat512 <wndus0958@naver.com>
2023-03-05 22:24:25 +0900

    삼총사

--- programmers/Lv.1/main.js
+++ programmers/Lv.1/main.js
@@ -10,20 +10,9 @@
     // 3개의 정수의 합이 0이 되는 경우의 수
     let answer = 0;
     for (let i = 0; i < number.length; i++) {
-      // 0 1
       for (let j = i + 1; j < number.length; j++) {
-        // 1 2 3 4
         for (let k = j + 1; k < number.length; k++) {
-          if (k === number.length) break;
-          // 2 3 4 5
           if (number[i] + number[j] + number[k] === 0) {
-            // [-2, 3, 0, 2, -5]
-            // (0, 1, 2) (0, 1, 3) (0, 1, 4)
-            // (0, 2, 3) (0, 2, 4)
-            // (0, 3, 4)
-            // (1, 2, 3) (1, 2, 4)
-            // (1, 3, 4)
-            // (3, 4, 5)
             answer++;
             continue;
           }
