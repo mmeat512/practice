ca8911151a3fcdddee59060e67caa3d9c71689bc
mmeat512 <wndus0958@naver.com>
2023-11-27 21:10:12 +0900

    init: 백준 프로젝트 초기설정

---
+++ baekjoon/submit.js
@@ -0,0 +1,12 @@
+// https://wonyoung2257.tistory.com/36
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString(); // 파일시스템을 이용하여 입력값을 받아오는 코드
+
+solution(inputN);
+
+function solution(inputN) {
+  /**
+   * 문제 풀이
+   */
+  console.log('정답출력');
+}
