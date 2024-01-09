33a710f0d7c28fbd60ab5e939b862309a8b8adab
mmeat512 <wndus0958@naver.com>
2024-01-09 20:37:53 +0900

    feat: 숨바꼭질

--- baekjoon/class3/1797.js
+++ baekjoon/class3/1797.js
@@ -1,61 +1,22 @@
 const oneLine = require('../common/oneLine');
 
-/**
- *
- * BFS
- *
- * @param {*} input
- */
 function solution(input) {
   const [start, end] = input.split(' ').map(Number);
-  let queue = [start];
-  let time = 0;
+  const visit = Array(100001).fill(false);
+  let queue = [[start, 0]];
+  visit[start] = true;
 
-  //   while (!queue.includes(end)) {
-  //     time += 1;
-  //     const arr = [];
-  //     const length = queue.length;
+  while (queue.length > 0) {
+    const [number, time] = queue.shift();
 
-  //     for (let i = 0; i < length; i++) {
-  //       const num = queue.shift();
-  //       arr.push(num - 1, num + 1, num * 2);
-  //     }
+    if (number === end) return time;
 
-  //     queue = queue.concat(arr);
-  //   }
-
-  const visit = [];
-  const lastNumber = 100000;
-
-  while (!visit[end]) {
-    const length = queue.length;
-
-    for (let i = 0; i < length; i++) {
-      const num = queue.shift();
-
-      const back = num - 1;
-      const go = num + 1;
-      const jump = num * 2;
-
-      if (!visit[back] && back >= 0 && back <= lastNumber) {
-        visit[back] = true;
-        queue.push(back);
-      }
-
-      if (!visit[go] && go >= 0 && go <= lastNumber) {
-        visit[go] = true;
-        queue.push(go);
-      }
-
-      if (!visit[jump] && jump >= 0 && jump <= lastNumber) {
-        visit[jump] = true;
-        queue.push(jump);
+    for (next of [number + 1, number - 1, number * 2]) {
+      if (!visit[next] && next >= 0 && next <= 100000) {
+        visit[number] = true;
+        queue.push([next, time + 1]);
       }
     }
-    time += 1;
   }
-
-  return time;
 }
-
 oneLine(solution);
