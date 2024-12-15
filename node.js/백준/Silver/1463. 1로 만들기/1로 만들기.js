cf14b636cea11963c4791eb9d11dfbeafb7a30f1
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2024-12-15 22:55:00 +0900

    [Silver III] Title: 1로 만들기, Time: 404 ms, Memory: 105080 KB -BaekjoonHub

---
+++ node.js/백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString();
+
+const num = Number(input);
+const dp = Array.of(num + 1);
+dp[0] = dp[1] = 0;
+
+/*
+  정수 X 를 1로 만드는 방법.
+  1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
+  2. X가 2로 나누어 떨어지면, 2로 나눈다.
+  3. 1을 뺀다. 
+
+  최솟값을 구하여라.
+*/
+
+function solution(num) {
+  /**
+   * 문제 풀이
+   */
+  // count 이기 때문에 +1 해주기!
+  if (dp[num] === undefined) {
+    if (num % 6 === 0) {
+      dp[num] =
+        Math.min(
+          Math.min(solution(num / 2), solution(num / 3)),
+          solution(num - 1),
+        ) + 1;
+    } else if (num % 3 === 0) {
+      dp[num] = Math.min(solution(num / 3), solution(num - 1)) + 1;
+    } else if (num % 2 === 0) {
+      dp[num] = Math.min(solution(num / 2), solution(num - 1)) + 1;
+    } else {
+      dp[num] = solution(num - 1) + 1;
+    }
+  }
+  return dp[num];
+}
+
+console.log(solution(num));
